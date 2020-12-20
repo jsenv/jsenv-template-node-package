e4d193a37d23a1ad40f199882ec9a37b41306a43
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-20 16:03:28 +0100

    add comment in files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,29 @@
+/**
+ * Eslint does not support esmodule in the config file. For that reason
+ * this file ends with the .cjs extension.
+ *
+ * This file uses eslint config from "@jsenv/eslint-config"
+ * And configure some params.
+ */
+
 const { createEslintConfig } = require("@jsenv/eslint-config")
 
 const config = createEslintConfig({
   projectDirectoryUrl: __dirname,
+
   importResolutionMethod: "import-map",
+  importMapFileRelativeUrl: "./import-map.importmap",
+
+  // "node" and "browser" params tells ESLint where our files will be executed.
+  // ESLint will configure the available global variables according to this param.
+  // Here it means ESLint could report an error like "window" is not defined
+  // but not "global" is not defined.
+  browser: false,
+  node: true,
+
+  // prettier param tells we are using prettier. It will disable all eslint rules
+  // already handled by prettier.
+  prettier: true,
 })
 
 module.exports = config
--- .github/release/publish-package.js
+++ .github/release/publish-package.js
@@ -1,10 +1,10 @@
-// import { publishPackage } from "@jsenv/package-publish"
+import { publishPackage } from "@jsenv/package-publish"
 
-// await publishPackage({
-//   projectDirectoryUrl: new URL("../../", import.meta.url),
-//   registriesConfig: {
-//     "https://registry.npmjs.org": {
-//       token: process.env.NPM_TOKEN,
-//     },
-//   },
-// })
+await publishPackage({
+  projectDirectoryUrl: new URL("../../", import.meta.url),
+  registriesConfig: {
+    "https://registry.npmjs.org": {
+      token: process.env.NPM_TOKEN,
+    },
+  },
+})
