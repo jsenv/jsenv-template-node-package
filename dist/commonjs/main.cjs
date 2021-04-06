1ef44d18a9709b348556df421761a66b50110a73
Damien Maillard <damien.maillard@dailymotion.com>
2021-04-06 11:49:56 +0200

    Update files

---
+++ dist/commonjs/main.cjs
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const message = "Hello world";
+
+const greet = () => {
+  return message;
+};
+
+const greetAsync = async () => {
+  return message;
+};
+
+exports.greet = greet;
+exports.greetAsync = greetAsync;
+
+//# sourceMappingURL=main.cjs.map
