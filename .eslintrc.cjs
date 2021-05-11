0860af4682dca4acbd1711eae1de581e5e510b11
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-11 09:57:51 +0200

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,61 +2,92 @@
  * Eslint does not support esmodule in the config file. For that reason
  * this file ends with the .cjs extension.
  *
- * This file uses eslint config from "@jsenv/eslint-config"
- * And configure some params.
+ * This file uses "@jsenv/eslint-config" to configure ESLint
+ * https://github.com/jsenv/jsenv-eslint-config#eslint-config
+ *
  */
 
-const { createEslintConfig } = require("@jsenv/eslint-config")
+const {
+  composeEslintConfig,
+  eslintConfigBase,
+  eslintConfigForPrettier,
+  eslintConfigToPreferExplicitGlobals,
+  jsenvEslintRules,
+  jsenvEslintRulesForImport,
+} = require("@jsenv/eslint-config")
 
-const eslintConfig = createEslintConfig({
-  projectDirectoryUrl: __dirname,
-
-  // Tell ESLint to use the importmap to resolve imports.
-  // Read more in https://github.com/jsenv/jsenv-node-module-import-map#Configure-vscode-and-eslint-for-importmap
-  importResolutionMethod: "import-map",
-  importMapFileRelativeUrl: "./importmap.dev.importmap",
+const eslintConfig = composeEslintConfig(
+  eslintConfigBase,
 
   // Files in this repository are all meant to be executed in Node.js
   // and we want to tell this to ESLint.
   // As a result ESLint can consider `window` as undefined and `global`
   // as an existing global variable.
-  // To do that we pass browser: false, node:true
-  browser: false,
-  node: true,
-
-  // prettier param tells we are using prettier. It will disable all eslint rules
-  // already handled by prettier.
-  prettier: true,
-})
-
-// Example of code changing the ESLint configuration to enable a rule:
-// config.rules['prefer-const'] = ['error']
-
-// disable commonjs globals by default
-// (package is "type": "module")
-Object.assign(eslintConfig.globals, {
-  __filename: "off",
-  __dirname: "off",
-  require: "off",
-})
-
-eslintConfig.overrides.push({
-  files: ["**/*.cjs"],
-  // inside *.cjs files. restore commonJS "globals"
-  globals: {
-    __filename: true,
-    __dirname: true,
-    require: true,
-  },
-  // inside *.cjs files, use commonjs module resolution
-  settings: {
-    "import/resolver": {
-      [Object.keys(eslintConfig.settings["import/resolver"])[0]]: {
-        node: true,
-        commonJsModuleResolution: true,
-      },
+  {
+    env: {
+      node: true,
     },
   },
-})
+
+  // Reuse jsenv eslint rules
+  {
+    rules: {
+      ...jsenvEslintRules,
+      // Example of code changing the ESLint configuration to enable a rule:
+      // 'prefer-const':  ['error']
+    },
+  },
+
+  // Enable import plugin
+  {
+    plugins: ["import"],
+    settings: {
+      "import/resolver": {
+        // Tell ESLint to use the importmap to resolve imports.
+        // Read more in https://github.com/jsenv/jsenv-node-module-import-map#Configure-vscode-and-eslint-for-importmap
+        "@jsenv/importmap-eslint-resolver": {
+          projectDirectoryUrl: __dirname,
+          importMapFileRelativeUrl: "./importmap.dev.importmap",
+          node: true,
+        },
+      },
+    },
+    rules: jsenvEslintRulesForImport,
+  },
+
+  // package is "type": "module" so:
+  // 1. disable commonjs globals by default
+  // 2. Re-enable commonjs into *.cjs files
+  {
+    globals: {
+      __filename: "off",
+      __dirname: "off",
+      require: "off",
+    },
+    overrides: [
+      {
+        files: ["**/*.cjs"],
+        // inside *.cjs files. restore commonJS "globals"
+        globals: {
+          __filename: true,
+          __dirname: true,
+          require: true,
+        },
+        // inside *.cjs files, use commonjs module resolution
+        settings: {
+          "import/resolver": {
+            node: {},
+          },
+        },
+      },
+    ],
+  },
+
+  eslintConfigToPreferExplicitGlobals,
+
+  // We are using prettier, disable all eslint rules
+  // already handled by prettier.
+  eslintConfigForPrettier,
+)
 
 module.exports = eslintConfig
